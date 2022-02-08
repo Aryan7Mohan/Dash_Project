1c71830ab964c3520686ca1762d3001e47b73f16
Aryan Mohan <aryan.mohan7@gmail.com>
2022-02-09 03:27:15 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,8 @@
+import dash
+
+
+app = dash.Dash(__name__, suppress_callback_exceptions=True,
+                meta_tags=[{'name': 'viewport',
+                            'content': 'width=device-width, initial-scale=1.0'}]
+                )
+server = app.server
